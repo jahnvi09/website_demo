ad0b4667393568abcef7a684771b49c986b6b5a3
jahnvi09 <jrekhi2002@gmail.com>
2025-03-03 19:27:30 -0500

    neww

--- src/components/Quickstart.js
+++ src/components/Quickstart.js
@@ -28,6 +28,7 @@ const QuickStart = () => {
               <li><span className="checkmark">✅</span><span className="quickstart-text"> Manage Deadlines – View upcoming tasks and reminders.</span></li>
               <li><span className="checkmark">✅</span><span className="quickstart-text"> Assignment Tracker – Keep track of progress and due dates.</span></li>
               <li><span className="checkmark">✅</span><span className="quickstart-text"> Take Notes – Save and download study notes.</span></li>
+              <li><span className="checkmark">✅</span><span className="quickstart-text">Upload assignment files, study notes, or course materials –  preview, edit, or remove them.</span></li>
               <li><span className="checkmark">✅</span><span className="quickstart-text"> Explore Resources – Access study guides and career help.</span></li>
             </ul>
 
